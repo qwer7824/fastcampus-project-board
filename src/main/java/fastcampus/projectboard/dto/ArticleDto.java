f59aacb4c4ebb030666959bffec5590e1b9f64b6
dev <sh7824@naver.com>
2023-03-09 15:44:40 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/fastcampus/projectboard/dto/ArticleDto.java
@@ -0,0 +1,20 @@
+package fastcampus.projectboard.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * A DTO for the {@link fastcampus.projectboard.domain.Article} entity
+ */
+public record ArticleDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        String title,
+        String content,
+        String hashtag
+){
+
+    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
+    return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+    }
+}
---
+++ src/main/java/fastcampus/projectboard/service/ArticleService.java
@@ -0,0 +1,43 @@
+package fastcampus.projectboard.service;
+
+
+import fastcampus.projectboard.domain.type.SearchType;
+import fastcampus.projectboard.dto.ArticleDto;
+import fastcampus.projectboard.dto.ArticleUpdateDto;
+import fastcampus.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    return Page.empty();
+
+    }
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+    return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+
+    }
+
+    public void DeleteArticle(long articleId) {
+    }
+}
